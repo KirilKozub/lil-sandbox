221f221e509723d5f78837ba486151048abe2cc7
KirilKozub <66863161+KirilKozub@users.noreply.github.com>
2025-07-05 00:52:50 +0200

    Update firstUpdatedList.js

--- firstUpdatedList.js
+++ firstUpdatedList.js
@@ -1,11 +1,14 @@
 firstUpdated() {
+    this.updateItemIndices();
+
     this.addEventListener('requestToggle', (e) => {
       const { index } = e.detail;
       const items = this.items;
 
       if (this.singleOpen) {
+        const isAlreadyOpen = items[index]?.open;
         items.forEach((item, i) => {
-          item.open = (i === index);
+          item.open = (i === index) ? !isAlreadyOpen : false;
         });
       } else {
         const targetItem = items[index];
